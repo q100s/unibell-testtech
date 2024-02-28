ad1829a30e00fced1e803b30a958dcdbe64b5260
eduard <eduard42rus@mail.ru>
2024-02-28 14:43:37 +0700

    init commit

---
+++ src/test/java/com/example/unibelltestech/UnibellTestechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.unibelltestech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnibellTestechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
